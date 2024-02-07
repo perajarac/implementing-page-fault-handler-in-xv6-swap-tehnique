62a30f1ba164fc6e919b7ed2597e0bf5d67111b0
Petar <petarmijatovic99@gmail.com>
2024-02-07 22:49:32 +0100

    Dodato lepo

--- kernel/swap_p.h
+++ kernel/swap_p.h
@@ -1,7 +1,7 @@
 #define KERNB 0x80000000L
 #define PHYS (KERNB + 16*1024*1024)
 #define NULL 0
-#define INDEX(pa) (pa/4096)
+#define INDEX(pa) (((uint64)pa-(uint64)KERNB)/4096)
 #define MAXINT 0xffffffff
 
 #define TOTALMEM (PHYS-KERNB)
